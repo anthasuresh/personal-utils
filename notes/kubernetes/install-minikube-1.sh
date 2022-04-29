aa81f037ce783d96d8a35754332d503ce0a7e0fd
anthasuresh <anthasuresh.lab@gmail.com>
2022-04-29 03:25:32 -0400

    auto

---
+++ notes/kubernetes/install-minikube-1.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+echo -e "\nsudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common\n"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo -e "\ncurl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -\n"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+echo -e "\nsudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable'\n"
+sudo apt-get update -y
+echo -e "\nsudo apt-get update -y\n"
+echo -e "$(sudo apt-get install -y docker-ce)"
+echo -e "\nsudo apt-get install -y docker-ce\n"
+echo "$(sudo usermod -aG docker $USER && newgrp docker)"
+echo -e "\nsudo usermod -aG docker $USER && newgrp docker\n"
+git clone https://github.com/DamionGans/ubuntu-wsl2-systemd-script.git
+echo -e "\ngit clone https://github.com/DamionGans/ubuntu-wsl2-systemd-script.git\n"
+cd ubuntu-wsl2-systemd-script/
+echo -e "\ncd ubuntu-wsl2-systemd-script/\n"
+bash ubuntu-wsl2-systemd-script.sh
+echo -e "\nbash ubuntu-wsl2-systemd-script.sh\n"
+
+echo -e "Restart the LxssManager in Windows to initialize systemctl with WSL\n\t\tWindows-Key --> services \n\n\t\tThen continue with curl install-minikube-2.sh "
+
+
+
+
+
+
+
+
+
+
+
---
+++ notes/kubernetes/install-minikube-2.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+#Install Conntrack
+sudo apt install -y conntrack
+
+#Install Minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x ./minikube
+sudo mv ./minikube /usr/local/bin/
+minikube config set driver docker
+
+#Install Kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin
+kubectl config use-context minikube
+
+#Install HELM
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh 
+chmod 700 get_helm.sh 
+./get_helm.sh
+
+
+echo -e "\n\n\t\tOpen a new terminal and type minikube / kubectl\n\n"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
