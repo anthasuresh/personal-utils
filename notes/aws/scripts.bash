d98b2ec0c8a8e006ceaf32adcce30d9f3185be70
Suresh Anbarasan <822841+anthasuresh@users.noreply.github.com>
2023-03-21 21:33:53 -0400

    Update scripts.bash

--- notes/aws/scripts.bash
+++ notes/aws/scripts.bash
@@ -1,1 +1,3 @@
-curl "https://aws.amazon.com/api/dirs/items/search?item.directoryId=aws-products&sort_by=item.additionalFields.productCategory&size=250&item.locale=en_US" | jq '.items[].item.additionalFields| .productCategory,.productName,.productSummary' | paste - - - | column
+curl "https://aws.amazon.com/api/dirs/items/search?item.directoryId=aws-products&size=300&item.locale=en_US" | jq '.items[].item.additionalFields| .productCategory,.productName,.productSummary' | paste - - - | column
+
+curl "https://aws.amazon.com/api/dirs/items/search?item.directoryId=aws-products&size=300&item.locale=en_US" | jq '.items[].item.additionalFields| .productCategory,.productName,.productSummary' | paste -d, - - - | grep -v "\(Preview\)" | sort > /mnt/c/Users/antha/Downloads/aws-services_new.csv
