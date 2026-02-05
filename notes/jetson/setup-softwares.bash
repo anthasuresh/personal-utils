563acef10090e6c135211de9a78d70287390c39a
Suresh Anbarasan <822841+anthasuresh@users.noreply.github.com>
2026-02-05 04:45:08 -0500

    Add setup script for software installation and configuration

---
+++ notes/jetson/setup-softwares.bash
@@ -0,0 +1,40 @@
+function create_bash_alias(){
+  echo -e "alias l='ls -trla'" >> ~/.bash_aliases
+  echo -e "alias c=clear" >> ~/.bash_aliases
+  echo -e "cd ~/vtrash/" >> ~/.bash_aliases
+  echo -e "source ~/venvs/ai/bin/activate" >> ~/.bash_aliases
+}
+
+function do_pip_installs(){
+  sudo apt install python3-pip -y
+  sudo apt install python3.10-venv -y
+  sudo pip3 install jetson-stats
+}
+
+function create_directories(){
+  mkdir ~/venvs ~/vtrash
+  touch ~/.hushlogin
+}
+
+function setup_venv(){
+  python3 -m venv ai
+  [ -f ~/venvs/ai/bin/activate ] && source ~/venvs/ai/bin/activate || { echo "Error: venv missing" ; exit 1 ; }
+}
+
+function install_ollama() {
+   curl -fsSL https://ollama.com/install.sh | sh
+    
+   echo "Waiting for Ollama service to start..."
+   until curl -s http://localhost:11434/api/tags > /dev/null; do
+     sleep 1
+   done
+    
+   ollama pull qwen3:4b-instruct
+}
+
+
+do_pip_installs
+create_directories
+create_bash_alias
+install_ollama
+setup_venv
